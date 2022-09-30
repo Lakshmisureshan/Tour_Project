360f748cb754965476489cff6373ac6d3c735833
Lakshmisureshan <113295897+Lakshmisureshan@users.noreply.github.com>
2022-09-30 13:09:47 +0400

    1 file modified

--- src/App.js
+++ src/App.js
@@ -10,8 +10,20 @@ export default function App() {
 
   const fetchTours = async () => {
     setLoading(true);
+    try {
+      const response = await fetch(url);
+      const tours = await response.json();
+      setLoading(false);
+      setTours(tours);
+    } catch (error) {
+      setLoading(false);
+      console.log(error);
+    }
   };
 
+  useEffect(() => {
+    fetchTours();
+  }, []);
   if (loading) {
     return (
       <main>

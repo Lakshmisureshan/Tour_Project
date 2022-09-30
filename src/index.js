8ec679779bc9d5e70c9e9572ab410b5b72597d7a
Lakshmisureshan <113295897+Lakshmisureshan@users.noreply.github.com>
2022-09-30 14:59:34 +0400

    3 files modified, 1 added

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React, { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-
+import './index.css';
 import App from './App';
 
 const rootElement = document.getElementById('root');
